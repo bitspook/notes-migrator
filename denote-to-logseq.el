42b28a775c338f48436f58a52f225a8d903ffaec
hapster <o.rojon@posteo.net>
2022-10-06 11:08:52 +0200

    nm/denote-to-logseq: (temporarily) add outline delimiters
    ;;; et cetera help keep the document structured.  Will use that so I don't get lost

--- denote-to-logseq.el
+++ denote-to-logseq.el
@@ -1,3 +1,16 @@
+;;; Global variables
+
+
+;;; Copy all files from DENOTE-DIRECTORY to LOGSEQ-DIRECTORY
+
+
+;;; In LOGSEQ-DIRECTORY, convert denote links to logseq linksh
+
+;;;; List elements in LOGSEQ-DIRECTORY
+
+;;;; For each link: check for an element in listed elements
+
+;;;; Get TITLE of said element
 (defun nm--convert-denote-links-to-logseq (filename)
   (let ((denote-link-rx (rx (and (group "[[denote:" (= 8 num))
 				 (group	"T" (= 6 num))
