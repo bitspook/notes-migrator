ef7f199c978838f51c40b36514687d3254afe1e5
hapster <o.rojon@posteo.net>
2022-10-08 23:52:54 +0200

    nm/denote-to-logseq: Getting closer I guess?
    So I have written NM--CONVERT-DENOTE-LINKS-TO-LOGSEQ that does most of the things I
    want corrctly, whereby I mean that all values in the LET-expressions are correct.
    However, there is an issue with matching and replacing that I don't quite understand.
    
    Currently, I have one magic variable baked in, MY-DENOTE-DIRECTORY.  I will have to
    find a way how to eliminate it, though I think this is less important than the
    matching/replacing part.
    
    Also, the file is a total mess overall, will have to clean up soon™.

--- denote-to-logseq.el
+++ denote-to-logseq.el
@@ -1,4 +1,3 @@
-;;;; Migrating notes from DENOTE to LOGSEQ format (including links)
 ;;; Global variables which are required
 (defvar my-denote-directory nil)
 (defvar my-logseq-directory nil)
@@ -7,15 +6,17 @@
 (setq my-logseq-directory "~/notes/test-logseq/")
 
 ;;; Copy contents of DENOTE directory to LOGSEQ directory
-(defun nm--copy-files-from-to (from to)
-  (let ((contents (directory-files from t "org$")))
+(defun nm--copy-org-files-from-to (src dest)
+  "Copy all Org files from SRC to DEST.
+SRC is supposed to be denote's, DEST logseq's notes directory."
+  (let ((contents (directory-files src t "org$")))
     (mapcar #'(lambda (file)
-		(copy-file file to t))
+		(copy-file file dest t))
 	    contents)))
 
 ;;; Find a file based on denote ID
 (with-temp-buffer
-  (insert-file (car (directory-files my-logseq-directory t "20221006T215838")))
+  (insert-file (car (directory-files my-logseq-directory t "20221007T210006")))
   (buffer-substring (point-min) (point-max)))
 
 ;;; Get an Org mode file's TITLE
@@ -25,33 +26,50 @@
   (let* ((key (org-collect-keywords '("TITLE")))
 	 (string (cadar key)))
     string))
-;;; Replace buffer contents via regexp
-(setq one-file (cadr (directory-files my-denote-directory t "org$")))
+
+(defun nm--convert-links-denote-to-logseq-one-file (file)
+  "In FILE, convert all denote links to logseq links.
+(1) In FILE, look for an expression matching denote-link
+regexp.  (2) Find the linked-to file using denote's
+identifier.  (3) Open the linked-to file and get its title.
+(4) Using the linked-to file's title, replace the
+denote-link-format with logseq's.  REPEAT"
+
+  
+  
+  )
 
 
-(let* ((denote-link-rx (rx (and (group "[[denote:" (= 8 num))
-				(group	"T" (= 6 num))
-				(group "][" (* (or ascii nonascii)))
-				(group	"]]")))))
-  (with-temp-buffer
-    (insert-file one-file)
-    (let* ((note-title (cadar (org-collect-keywords '("TITLE"))))
-	   (logseq-link (concat "[[" note-title "]]")))
-      (replace-regexp denote-link-rx logseq-link)
-      (buffer-substring (point-min) (point-max)))))
-
+;;; Apply nm--convert-links to all files
+(defun nm--convert-links-denote-to-logseq (dir)
+  "In DIR, converts all denote links to logseq links."
+  (mapcar #'(lambda (file)
+	      (nm--convert-links-denote-to-logseq-one-file file))
+	  (directory-files dir t (rx "org" line-end))))
 ;;; Replace denote-link with logseq-link, first try
+(setq one-file (car (directory-files my-denote-directory t "org$")))
+
 (defun nm--convert-denote-links-to-logseq (filename)
   (let* ((denote-link-rx (rx (and (group "[[denote:" (= 8 num))
 				  (group "T" (= 6 num))
-				  (group "][" (* (or ascii nonascii)))
+				  (group "][" (* (or alnum print)))
 				  (group "]]")))))
     (with-temp-buffer
       (insert-file filename)
-      (let* ((note-title (cadar (org-collect-keywords '("TITLE"))))
-	     (logseq-link (concat "[[" note-title "]]")))
-	(replace-regexp denote-link-rx logseq-link)
-	(buffer-substring (point-min) (point-max))))))
+      (while (re-search-forward denote-link-rx nil t)
+	  (let* ((element (org-element-copy (org-element-context)))
+		 ;; (parsed-buffer (org-element-parse-buffer))
+		 (linked-id (org-element-property :path element))
+		 (linked-file (car (directory-files my-denote-directory t linked-id)))
+		 (linked-title (with-temp-buffer
+				 (insert-file linked-file)
+				 (org-mode)
+				 (cadar (org-collect-keywords '("TITLE")))))
+		 (logseq-format (concat "[[" linked-title "]]")))
+	    (replace-match logseq-format)))
+      (buffer-substring (point-min) (point-max)))))
+
+(nm--convert-denote-links-to-logseq one-file)
 
 ;;; Other stuff
 (defun nm--convert-denote-links-to-logseq (filename)
