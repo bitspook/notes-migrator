b50c1812fb82b88c58a7ba4ac3fb6ebbc53af7b0
Charanjit Singh <bitspook@protonmail.com>
2022-10-11 14:18:37 +0530

    feat: basic denote to logseq migration
    Instead of solving the regexp-replace problem (which I am sure would be
    interesting and difficult), I chose to take a slightly different approach of
    deleting the existing denote org-link and inserting a logseq-link in its place.
    
    Also took some liberties to move some code around and rename some things, sorry
    @hapst3r
    
    A call to
    
    ```lisp
    (let ((logseq-dir "/tmp/logseq"))
      (nm--migrate-denote-to-logseq))
    ```
    
    works for all my notes, so I assume it is good enough for initial iteration.

--- denote-to-logseq.el
+++ denote-to-logseq.el
@@ -1,61 +1,76 @@
-;;; Global variables which are required
+;;; package -- Utilities to migrate denote notes to logseq
+;;
+;; Author: hapst3r
+;; Version: 0.1
+;; URL: https://github.com/bitspook/notes-migrator/denote-to-logseq.el
+;; Keywords: Denote, Logseq
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "27.2") (denote "1.0.0")
+;;
+;;; Commentary:
+;; A WIP package for migrating Denote notes to Logseq
+;;
+;;; Code:
+
 (require 'denote)
 
-(defvar logseq-directory nil)
-
-(setq logseq-directory "~/notes/test-logseq/")
+(defvar logseq-directory "~/notes/test-logseq/")
 
 ;;; Copy contents of DENOTE directory to LOGSEQ directory
 (defun nm--copy-org-files-from-to (src dest)
   "Copy all Org files from SRC to DEST.
 SRC is supposed to be denote's, DEST logseq's notes directory."
-  ;; TODO: Rewrite using mechanisms provided by DENOTE  
+  ;; TODO: Rewrite using mechanisms provided by DENOTE
   (let ((contents (directory-files src t "org$")))
     (mapcar #'(lambda (file)
-		(copy-file file dest t))
-	    contents)))
+		            (copy-file file dest t))
+	          contents)))
 
-;;; Find a file based on denote ID and get its TITLE
-(let* ((denote-directory logseq-directory)
-       (tmp-id "20220909T083622")
-       (file (denote-get-path-by-id tmp-id)))
-  (denote-link--file-type-regexp (denote-retrieve-title-value file 'org)))
-
-
-
-(defun nm--convert-links-denote-to-logseq-one-file (file)
-  "In FILE, convert all denote links to logseq links.
-Use the following steps:
-(1) In FILE, look for an expression matching denote-link regexp.
-(2) Find the linked-to file using denote's identifier.
-(3) Open the linked-to file and get its title.
-(4) Using the linked-to file's title, replace the
-denote-link-format with logseq's.
-(5) REPEAT."
+(defun nm--convert-denote-links-to-logseq ()
+  "Convert all denote links to logseq links in current buffer.
+Current-buffer should be in `org-mode'."
   (let ((denote-link-rx (rx "[[denote:")))
+    (while (re-search-forward denote-link-rx nil t)
+	    (let* ((element (org-element-copy (org-element-context)))
+	           (contents (org-element-property :format element))
+	           (linked-id (org-element-property :path element))
+	           (linked-file (denote-get-path-by-id linked-id))
+	           (linked-title (denote-retrieve-title-value linked-file 'org))
+	           (logseq-link (format "[[%s]]" linked-title)))
+        (delete-region (org-element-property :begin element)
+                       (org-element-property :end element))
+	      (insert logseq-link)))))
+
+(defun nm--migrate-denote-file-to-logseq (file)
+  "Convert denote note FILE to logseq.
+Copy the file from `denote-directory' to `logseq-directory' and
+convert all denote links to logseq."
+  (let ((dest (expand-file-name
+               (string-replace denote-directory "" file)
+               logseq-directory)))
     (with-temp-buffer
       (insert-file-contents file)
-      (if (re-search-forward denote-link-rx nil t)
-	(let* ((element (org-element-copy (org-element-context)))
-	       (contents (org-element-property :format element))
-	       (linked-id (org-element-property :path element))
-	       (linked-file (denote-get-path-by-id linked-id))
-	       (linked-title (denote-retrieve-title-value linked-file 'org))
-	       (logseq-link (format "[[%s]]" linked-title)))
-	  ;; (replace-string (format "[[denote:%s][%s]]" linked-id ) logseq-link)
-	  contents))
-      ;; (buffer-substring (point-min) (point-max))
-      )))
+      (org-mode)
+      (nm--convert-denote-links-to-logseq)
+      (write-file dest))))
 
-(nm--convert-links-denote-to-logseq-one-file "~/notes/test-logseq/20221004T094518--kontrollstrukturen-iteration__cl_syntax_fluss.org")
+(defun nm--ensure-dir-path (path)
+  "Ensure that PATH is a directory path.
+A path is directory path if it ends with a \"/\". Might not work
+on windows."
+  (if (string-match-p (rx "/" line-end) path)
+      path
+    (concat path "/")))
 
 ;;; Apply nm--convert-links to all files
-(defun nm--convert-links-denote-to-logseq (dir)
-  "In DIR, converts all denote links to logseq links."
-  (mapcar #'(lambda (file)
-	      (nm--convert-links-denote-to-logseq-one-file file))
-	  (directory-files dir t (rx "org" line-end))))
-
+(defun nm--migrate-denote-to-logseq ()
+  "Migrate all denote notes logseq.
+Migrate all org files in `denote-directory' to logseq notes in
+`logseq-directory'."
+  (let* ((denote-directory (nm--ensure-dir-path denote-directory))
+         (logseq-directory (nm--ensure-dir-path logseq-directory))
+         (denote-org-files (directory-files denote-directory t "\.org$")))
+    (mapcar #'nm--migrate-denote-file-to-logseq denote-org-files)))
 
 ;;; Other stuff
 (defun test-fn (in-file out-file)
